f24fcd4987b387e1be374393f260a9dc3fdc932f
borgesjose <jose.borges90@hotmail.com>
2022-09-24 21:20:24 -0300

    versao24-09-2022

---
+++ Coupled tanks/Pratical-codes/Cilindrical_Tank_SISO_MQ.m
@@ -0,0 +1,10 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%  
+% Universidade Federal do Piauí                       %
+% Campus Ministro Petronio Portela                    %
+% Copyright 2022 -José Borges do Carmo Neto-          %
+% @author José Borges do Carmo Neto                   %
+% @email jose.borges90@hotmail.com                    %
+% cilindrical tank MQ Identification                  %
+%                                                     %
+%  -- Version: 1.0  - 25/09/2022                      %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
