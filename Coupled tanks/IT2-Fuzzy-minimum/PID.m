17cccbaa96957e21b67d4906c4e5f026bc7f9241
borgesjose <jose.borges90@hotmail.com>
2022-12-17 23:09:43 -0300

    17-12-22

---
+++ Coupled tanks/IT2-Fuzzy-minimum/PID.m
@@ -0,0 +1,48 @@
+function [Kc,Ti,Td] = PID(Ctype),
+
+      if (Ctype == 'ZN')
+
+            Kc = 0.27589;
+            Ti =  1.5;
+            Td = 0.375;       
+        end;
+        
+        if (Ctype == 'CC')
+            Kc = 0.005;
+            Ti = 0.50;
+            Td = 0.090;            
+        end;
+        
+        if (Ctype == 'AT')
+            Kc = 0.10769;
+            Ti = 4.9011;
+            Td = 1.2253;             
+            
+        end;   
+        
+        if (Ctype == 'FG')
+             Kc = 0.488;
+             Ti = 27.968;
+             Td = 0.51381;
+             
+%             K = AT_PID_FG(Am,L,a,b,c);
+%             
+%             Kc = K(1);
+%             Ti = Kc/K(2);
+%             Td = K(3)/Kc;
+
+
+            
+        end; 
+        
+        if(Ctype == 'PR')
+            disp("Selecione um controlador: ZN , CC, AT ") 
+            %SINTONIA PROFESSOR:
+            Kc = 0.37222;
+            Ti = 1.5;
+            Td = 0.375;
+            %Td = 0.0;
+        end; 
+   
+
+            
